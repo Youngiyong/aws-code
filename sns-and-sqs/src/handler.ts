25eba5a4410e99ea9bd1cae3c16c8b5f1ae623ae
Youngiyong <youn9354@naver.com>
2021-08-15 17:14:53 +0900

    sns testing

--- sns-and-sqs/src/handler.ts
+++ sns-and-sqs/src/handler.ts
@@ -1,30 +1,56 @@
 import { Handler, Context } from "aws-lambda";
+// import { requestGetSNSCheck } from './model'
 
 interface Response {
   statusCode: number;
   body: string;
 }
 
+
+const snsEvent: Handler = async (event: any, context: Context) => {
+    const response  = {
+          statusCode: 200,
+          body: {
+            message: "한안렬 븅신1234"
+          }
+        }
+    console.log("snsEvent Check")
+    return JSON.stringify(response)
+}
+
+
 const sns: Handler = async (event: any, context: Context) => {
-  const response: Response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello SNS!"
-    })
-  };
-  return response;
-};
+  
+  await snsEvent(event, context)
 
-const sqs: Handler = async (event: any, context: Context) => {
-  const response: Response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello SQS!"
-    })
-  };
-  return response;
 };
 
 
 
-export { sns, sqs };
+const lambdaHook: Handler = async (event: any, context: Context) => {
+	console.log('Function name: ', context.functionName)
+	console.log('Remaining time: ', context.getRemainingTimeInMillis())
+
+	context.callbackWaitsForEmptyEventLoop = false;
+
+	// -------------------------------------------------------------------
+	console.log("lambda Hook Start", event);
+
+  for(var i=0; i<10; i++){
+
+    await setTimeout(function() {
+      sns(event, Context)
+        .then((res)=> {
+        console.log("=========================> 1." , res)
+        }).catch((err)=> {
+        console.log("=========================> 2.", err)
+        })
+    }, 30000); //wait 30 second
+  }
+
+  console.log("end lambdaHook")
+  
+};
+
+
+export { sns,  lambdaHook, snsEvent };
---
+++ sns-and-sqs/src/model.ts
@@ -0,0 +1,6 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+// export const requestGetSNSCheck = async() => {
+// 	const url = `https://kwq7g2hln1.execute-api.ap-northeast-2.amazonaws.com/dev/sns`
+//     console.log(url)
+// 	return await axios.get(url)
