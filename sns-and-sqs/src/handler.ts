1ea054113b0d83fc65033fd40bd89aa0e7f3c502
Youngiyong <youn9354@naver.com>
2021-08-16 23:20:18 +0900

    test

--- sns-and-sqs/src/handler.ts
+++ sns-and-sqs/src/handler.ts
@@ -8,72 +8,75 @@ interface Response {
 
 
 const snsEvent: Handler = async (event: any, context: Context) => {
-    const response  = {
-          statusCode: 200,
-          body: {
-            message: "한안렬 븅신1234"
-          }
-        }
-    console.log("snsEvent Check")
-    return JSON.stringify(response)
+  setTimeout(async ()=>{
+      console.log("setTimeout", await sns(event, context))
+  }, 2000)
+  console.log("snsEvent The End")
 }
 
 
 const sns: Handler = async (event: any, context: Context) => {
   
-  return await snsEvent(event, context)
+  return "SNS 입니다."
 
 };
 
 
-
-const lambdaHook: Handler = async (event: any, context: Context) => {
-	console.log('Function name: ', context.functionName)
-	console.log('Remaining time: ', context.getRemainingTimeInMillis())
-
-	context.callbackWaitsForEmptyEventLoop = false;
-
-	// -------------------------------------------------------------------
-	console.log("lambda Hook Start", event);
-
-    await setTimeout(async () =>  {
-       console.log("왜안돼?")
-       console.log("setTimeout", await sns(event, context))
-    }, 3000)
-    
-    console.log("2",await sns(event, context))
-
-    await setTimeout(function () {
-      console.log("setTimeout2", sns(event, context))
-    }, 3000)
-
-    console.log("4",await sns(event, context))
-  // await setTimeout(async function() {
-  //   for(var i=0; i<10; i++){
-  //     console.log(i)
-  //     let data = null
-  //     await sns(event, Context).then((res)=>{
-  //       console.log(res)
-  //       data=res
-  //     }).catch((err)=> {
-  //       console.log(err)
-  //     })
-  //     console.log(data)
-      // await setTimeout(function() {
-      //   sns(event, Context)
-      //     .then((res)=> {
-      //     console.log("=========================> 1." , res)
-      //     }).catch((err)=> {
-      //     console.log("=========================> 2.", err)
-      //     })
-      // }, 30000); //wait 30 second
-    // }
-  // }, 30000); //wait 30 second
-
-
-  console.log("end lambdaHook")
+const lambdaHook: Handler = function(event: any, context: Context){
   
-};
+  setTimeout(async ()=>{
+    console.log("setTimeout", await sns(event, context))
+  }, 30000)
+
+  console.log("LambadHook The End")
+}
+// const lambdaHook: Handler = async (event: any, context: Context) => {
+// 	console.log('Function name: ', context.functionName)
+// 	console.log('Remaining time: ', context.getRemainingTimeInMillis())
+
+// 	context.callbackWaitsForEmptyEventLoop = false;
+
+// 	// -------------------------------------------------------------------
+// 	console.log("lambda Hook Start", event);
+
+//     await setTimeout(async () =>  {
+//        console.log("왜안돼?")
+//        console.log("setTimeout", await sns(event, context))
+//     }, 3000)
+    
+//     console.log("2",await sns(event, context))
+
+//     await setTimeout(function () {
+//       console.log("setTimeout2", sns(event, context))
+//     }, 3000)
+
+//     console.log("4",await sns(event, context))
+//   // await setTimeout(async function() {
+//   //   for(var i=0; i<10; i++){
+//   //     console.log(i)
+//   //     let data = null
+//   //     await sns(event, Context).then((res)=>{
+//   //       console.log(res)
+//   //       data=res
+//   //     }).catch((err)=> {
+//   //       console.log(err)
+//   //     })
+//   //     console.log(data)
+//       // await setTimeout(function() {
+//       //   sns(event, Context)
+//       //     .then((res)=> {
+//       //     console.log("=========================> 1." , res)
+//       //     }).catch((err)=> {
+//       //     console.log("=========================> 2.", err)
+//       //     })
+//       // }, 30000); //wait 30 second
+//     // }
+//   // }, 30000); //wait 30 second
+
+
+//   console.log("end lambdaHook")
+  
+// };
 
 
 export { sns,  lambdaHook, snsEvent };
